4eb8153628c083c1a81c4b005d1a18d3d71ac228
Gauthier Provost <gauthier@kobol.io>
2019-07-23 17:07:46 +0800

    Update app and install script to use Python 3

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 INSTALL_PATH="/usr/local/"
 SYSTEMD_PATH="/etc/systemd/"
-DEPS="python-dev python-pip python-setuptools python-wheel python-configparser python-psutil libfreetype6-dev libjpeg-dev build-essential"
+DEPS="python3-dev python3-pip python3-setuptools python3-wheel python-configparser python3-psutil libfreetype6-dev libjpeg-dev build-essential"
 
 if [ "$(id -u)" != "0" ]; then
     echo "This script must be run as root!"
@@ -15,7 +15,7 @@ apt-get update
 apt-get install -y $DEPS
 
 echo "Installing luma.oled library"
-pip install --upgrade luma.oled
+pip3 install --upgrade luma.oled
 
 echo "Installing sys-oled files"
 cp -fv etc/sys-oled.conf  /etc
@@ -23,6 +23,6 @@ cp -frv bin "$INSTALL_PATH"
 cp -frv share "$INSTALL_PATH"
 cp -frv system "$SYSTEMD_PATH"
 
-echo "Enabling sys-oled at statup"
+echo "Enabling sys-oled at startup"
 systemctl daemon-reload
 systemctl enable sys-oled.service
