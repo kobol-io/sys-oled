61a62cc077a44fd51c1966bf13c93bb179d0e2a5
Gauthier Provost <gauthier@kobol.io>
2019-08-25 13:34:59 +0800

    Remove dependency on python-configparser dpkg
    In python3 configparser seems to be part of the core package therefore no need python-configparser dpkg which was actually for python2.
    
    Closed Issue #5

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 INSTALL_PATH="/usr/local/"
 SYSTEMD_PATH="/etc/systemd/"
-DEPS="python3-dev python3-pip python3-setuptools python3-wheel python-configparser python3-psutil libfreetype6-dev libjpeg-dev build-essential"
+DEPS="python3-dev python3-pip python3-setuptools python3-wheel python3-psutil libfreetype6-dev libjpeg-dev build-essential"
 
 if [ "$(id -u)" != "0" ]; then
     echo "This script must be run as root!"
